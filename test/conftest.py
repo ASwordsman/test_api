17f6aa403d96f297455b9c44a29896044b88f22d
wang hai kang <1184774275@qq.com>
2019-08-26 11:11:38 +0800

    优化控制台调试

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+from pprint import pprint
 
 from test_api.api.logins import Logins
 
@@ -6,6 +8,9 @@ from test_api.api.logins import Logins
 class WE(Logins):
 
     def test_login(self):
+        """
+        :return: 返回状态码 token
+        """
         response = self.login_admin()
         # print(self.header_print)
         # print(self.url)
@@ -15,10 +20,14 @@ class WE(Logins):
 
 test = WE()
 test.test_login()
-print(test.header_print)
-# test.test_login()
 @pytest.fixture()
 def login_code():
     # 登录返回值
     code, token = test.test_login()
     return code, token
+
+
+print(test.url)
+pprint(dict(test.header_print))
+pprint(json.loads(test.response_text))
+

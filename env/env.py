df01f1796259221d16b047a10a3e9b3f2e2862f3
ASwordman <1184774275@qq.com>
2020-02-24 11:38:10 +0800

    环境配置文件修改

--- env/env.py
+++ env/env.py
@@ -1,3 +1,5 @@
+import os
+
 import yaml
 import pprint
 
@@ -8,24 +10,24 @@ class Env(object):
 
     def __init__(self, env_name):
         self.env_name = env_name
-        self.abspath = settings.parent_path + '\\yaml_dict'
+        self.abspath = os.path.join(settings.parent_path, 'yaml_dict')
 
         self.env_dicts = self.choose_env()
 
     def choose_env(self):
         dirpath = ''
         if self.env_name == 'ys':
-            dirpath = self.abspath + '\\ys.yaml'
+            dirpath = os.path.join(self.abspath, 'ys.yaml')
         elif self.env_name == 'vs':
-            dirpath = self.abspath + '\\vs.yaml'
+            dirpath = os.path.join(self.abspath, 'vs.yaml')
 
         elif self.env_name == ' fs':
-            dirpath = self.abspath + '\\vs.yaml'
+            dirpath = os.path.join(self.abspath, 'vs.yaml')
         elif self.env_name == 'ws':
-            dirpath = self.abspath + '\\ws.yaml'
+            dirpath = os.path.join(self.abspath, 'ws.yaml')
 
         elif self.env_name == 'us':
-            dirpath = self.abspath + '\\us.yaml'
+            dirpath = os.path.join(self.abspath, 'us.yaml')
         else:
             pass
         with open(dirpath, 'r+') as f:
